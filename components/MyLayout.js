eff79992b779142c7e6a47ebf716cdd0ee9506dc
Gershwen <76742941+Gershwen@users.noreply.github.com>
2022-01-06 16:27:57 +0200

    changed crossOrigin

--- components/MyLayout.js
+++ components/MyLayout.js
@@ -12,11 +12,11 @@ const Layout = (props) => {
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         />
         {/* Fonts from Google fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;500;700&display=swap"
           rel="stylesheet"
